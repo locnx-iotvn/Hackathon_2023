26832205e60ef192f9ae301b0130c3bd0942d070
LocNguyen <74259097+locnx-iotvn@users.noreply.github.com>
2023-09-26 23:46:03 +0700

    Arduino
    Update limit switch

--- Arduino/lib/main.h
+++ Arduino/lib/main.h
@@ -7,9 +7,9 @@
 #define SERVO_PIN 12
 #define VALVE_PIN 11
 #define SUCKING_MOTOR_PIN 13
-#define LIMIT_SWITCH_X  10
-#define LIMIT_SWITCH_Y  9
-#define LIMIT_SWITCH_PRESS 1
+#define LIMIT_SWITCH_X  9
+#define LIMIT_SWITCH_Y  10
+#define LIMIT_SWITCH_PRESS 0
 
 // Define the stepper motors and the pins the will use
 AccelStepper stepper1(1, 2, 5); // (Type:driver, STEP, DIR)
@@ -53,6 +53,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y},
                         // "i2", "h2", "g2", "f2", "e2", "d2", "c2", "b2", "a2",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE}, 
@@ -62,6 +63,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y + ROW_DISTANCE}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y + ROW_DISTANCE},
                         // "i3", "h3", "g3", "f3", "e3", "d3", "c3", "b3", "a3",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE*2}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE*2}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE*2}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE*2}, 
@@ -71,6 +73,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y + ROW_DISTANCE*2}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y + ROW_DISTANCE*2},
                         // "i4", "h4", "g4", "f4", "e4", "d4", "c4", "b4", "a4",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE*3}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE*3}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE*3}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE*3}, 
@@ -80,6 +83,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y + ROW_DISTANCE*3}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y + ROW_DISTANCE*3},
                         // "i5", "h5", "g5", "f5", "e5", "d5", "c5", "b5", "a5",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE*4}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE*4}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE*4}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE*4}, 
@@ -89,6 +93,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y + ROW_DISTANCE*4}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y + ROW_DISTANCE*4},
                         // "i6", "h6", "g6", "f6", "e6", "d6", "c6", "b6", "a6",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE*5}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE*5}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE*5}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE*5}, 
@@ -98,6 +103,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y + ROW_DISTANCE*5}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y + ROW_DISTANCE*5},
                         // "i7", "h7", "g7", "f7", "e7", "d7", "c7", "b7", "a7",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE*6}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE*6}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE*6}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE*6}, 
@@ -107,6 +113,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y + ROW_DISTANCE*6}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y + ROW_DISTANCE*6},
                         // "i8", "h8", "g8", "f8", "e8", "d8", "c8", "b8", "a8",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE*7}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE*7}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE*7}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE*7}, 
@@ -116,6 +123,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y + ROW_DISTANCE*7}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y + ROW_DISTANCE*7},
                         // "i9", "h9", "g9", "f9", "e9", "d9", "c9", "b9", "a9",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE*8}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE*8}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE*8}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE*8}, 
@@ -125,6 +133,7 @@ int coordinatesChess[NUMBER_POSTION_CHESS][2] = {
                         {ORIGIN_X - COLUMN_DISTANCE*7, ORIGIN_Y + ROW_DISTANCE*8}, 
                         {ORIGIN_X - COLUMN_DISTANCE*8, ORIGIN_Y + ROW_DISTANCE*8},
                         // "i10", "h10", "g10", "f10", "e10", "d10", "c10", "b10", "a10",
+                        {ORIGIN_X,  ORIGIN_Y + ROW_DISTANCE*9}, 
                         {ORIGIN_X - COLUMN_DISTANCE*1, ORIGIN_Y + ROW_DISTANCE*9}, 
                         {ORIGIN_X - COLUMN_DISTANCE*2, ORIGIN_Y + ROW_DISTANCE*9}, 
                         {ORIGIN_X - COLUMN_DISTANCE*3, ORIGIN_Y + ROW_DISTANCE*9}, 
--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -9,13 +9,15 @@ double theta1, theta2;
 int stepper1Position, stepper2Position;
 
 String content = "";
-String data[NUMBER_DATA_SERIAL];
+String dataFromUart[NUMBER_DATA_SERIAL];
 int dataPlayChess[5];   // Status, Coordinates_X0, Coordinates_Y0, Coordinates_X1, Coordinates_Y1
+int dataPlayChess_Bk[5];   // Status, Coordinates_X0, Coordinates_Y0, Coordinates_X1, Coordinates_Y1
 uint8_t numberChessEat = 0;
 
 void startSetup();
 void resetPosition();
-void readDataFromUart();
+void readDataFromUart();\
+void convertDataUartToCoordinates();
 void playChess();
 void moveChess(int Coordinates_X0, int Coordinates_Y0, int Coordinates_X1, int Coordinates_Y1);
 void caculateRotationAngle(int x, int y);
@@ -29,15 +31,24 @@ void setup() {
 }
 
 void loop() {
+    if (digitalRead(LIMIT_SWITCH_X) != LIMIT_SWITCH_PRESS)
+    {
+      Serial.println("digitalRead(LIMIT_SWITCH_X)");
+    }
+    if (digitalRead(LIMIT_SWITCH_Y) != LIMIT_SWITCH_PRESS)
+    {
+      Serial.println("digitalRead(LIMIT_SWITCH_Y)");
+    }    
+    delay(1000);
+
   // Read data
-  readDataFromUart();
+  // readDataFromUart();
 
-  if ( bPlayChess == true)
-  {
-    playChess();
-    bPlayChess = false;
-  }
-
+  // if (bPlayChess == true)
+  // {
+  //   playChess();
+  //   bPlayChess = false;
+  // }
 }
 
 void startSetup()
@@ -48,17 +59,18 @@ void startSetup()
   // Start step motor 1
   stepper1.setMaxSpeed(4000);
   stepper1.setAcceleration(2000);
-  stepper1.setSpeed(100);
-  stepper1.setAcceleration(100);
+  // stepper1.setSpeed(100);
+  // stepper1.setAcceleration(100);
 
   // Start step motor 2
   stepper2.setMaxSpeed(4000);
   stepper2.setAcceleration(2000);
-  stepper2.setSpeed(100);
-  stepper2.setAcceleration(100);
+  // stepper2.setSpeed(100);
+  // stepper2.setAcceleration(100);
 
   // Start servo motor
   myservo.attach(SERVO_PIN);
+  controlServo(0);
 
   // Setup pin output for sucking motor
   pinMode(SUCKING_MOTOR_PIN, OUTPUT);
@@ -74,42 +86,55 @@ void startSetup()
 }
 
 void resetPosition() {
+  delay(500);
   // Homing step motor X
   while (digitalRead(LIMIT_SWITCH_X) != LIMIT_SWITCH_PRESS) {
-    stepper1.setSpeed(200);
+    stepper1.setSpeed(-130);
     stepper1.runSpeed();
-    stepper1.setCurrentPosition(-5420); // When limit switch pressed set position to 0 steps
+    // stepper1.setCurrentPosition(2000); // When limit switch pressed set position to 0 steps
   }
   delay(20);
 
-  stepper1.moveTo(0);
-  while (stepper1.currentPosition() != 0) {
-    stepper1.run();
-  }
+  // stepper1.moveTo(0);
+  // while (stepper1.currentPosition() != 0) {
+  //   stepper1.run();
+  // }
 
   // Homing step motor Y
   while (digitalRead(LIMIT_SWITCH_Y) != LIMIT_SWITCH_PRESS) {
-    stepper2.setSpeed(-1300);
+    stepper2.setSpeed(130);
     stepper2.runSpeed();
-    stepper2.setCurrentPosition(-5420); // When limit switch pressed set position to 0 steps
+    // stepper1.setCurrentPosition(100);
+  }
+
+  while (digitalRead(LIMIT_SWITCH_Y) != LIMIT_SWITCH_PRESS) {
+    stepper2.setSpeed(20);
+    stepper2.runSpeed();
+    // stepper1.setCurrentPosition(100);
+  }
+
+  while (digitalRead(LIMIT_SWITCH_X) != LIMIT_SWITCH_PRESS) {
+    stepper1.setSpeed(-20);
+    stepper1.runSpeed();
+    // stepper1.setCurrentPosition(2000); // When limit switch pressed set position to 0 steps
   }
   delay(20);
 
-  stepper2.moveTo(0);
-  while (stepper2.currentPosition() != 0) {
-    stepper2.run();
-  }
+  // stepper2.moveTo(0);
+  // while (stepper2.currentPosition() != 0) {
+  //   stepper2.run();
+  // }
 }
 
 void readDataFromUart()
 {
   if (Serial.available()) {
     content = Serial.readString(); // Read the incomding data from Processing
-    // Extract the data from the string and put into separate integer variables (data[] array)
     for (int i = 0; i < NUMBER_DATA_SERIAL; i++) {
       int index = content.indexOf(","); // locate the first ","
-      data[i] = content.substring(0, index).c_str(); //Extract the number from start to the ","
+      dataFromUart[i] = content.substring(0, index);
       content = content.substring(index + 1); //Remove the number from the string
+      Serial.println(dataFromUart[i]);
     }
 
     /*
@@ -119,49 +144,67 @@ void readDataFromUart()
     */
 
     bPlayChess = true;
-
-    // Convert Uart data to chess playing data
-    dataPlayChess[0] = atol(data[0].c_str());
-    for (int i = 0; i < NUMBER_POSTION_CHESS; ++i) {
-      // Coordinates X of the chess
-      if (posstionChess[i] == data[1])
-      {
-        dataPlayChess[0] = coordinatesChess[i][0];
-        dataPlayChess[1] = coordinatesChess[i][1];
-      }
-      // Coordinates Y of the chess
-      if (posstionChess[i] == data[2])
-      {
-        dataPlayChess[2] = coordinatesChess[i][0];
-        dataPlayChess[3] = coordinatesChess[i][1];
-        break;
-      }
-    }
-
-    Serial.println("Move from (" + String(dataPlayChess[0]) + "," + String(dataPlayChess[1]) + ") to (" + String(dataPlayChess[3]) + "," + String(dataPlayChess[4]) + ")");
   }
+}
+
+void convertDataUartToCoordinates()
+{
+  char dataFromUart_temp[3];
+  char posstionChess_temp[3];
+  strcpy(dataFromUart_temp, dataFromUart[0].c_str());
+
+  // Convert Uart data to chess playing data
+  Serial.println(dataFromUart[0]);
+  dataPlayChess[0] = atol(dataFromUart[0].c_str());
+
+  // Coordinates X of the chess
+  for (int i = 0; i < NUMBER_POSTION_CHESS; ++i) {
+    strcpy(posstionChess_temp, posstionChess[i].c_str());
+    if ((dataFromUart_temp[0] == posstionChess_temp[0]) && (dataFromUart_temp[1] == posstionChess_temp[1]) && (dataFromUart_temp[2] == posstionChess_temp[2]))
+    // if (posstionChess[i] == dataFromUart[1])
+    {
+      dataPlayChess[1] = coordinatesChess[i][0];
+      dataPlayChess[2] = coordinatesChess[i][1];
+      Serial.println(i);
+      break;
+    }
+  }
+  Serial.println(dataFromUart[0]);
+
+  // // Coordinates Y of the chess
+  // for (int i = 0; i < NUMBER_POSTION_CHESS; ++i) {
+  //   if (posstionChess[i] == dataFromUart[2])
+  //   {
+  //     dataPlayChess[3] = coordinatesChess[i][0];
+  //     dataPlayChess[4] = coordinatesChess[i][1];
+  //     Serial.println(i);
+  //     break;
+  //   }
+  // }
+  // Serial.println(dataFromUart[0]);
 }
 
 void playChess()
 {
+  convertDataUartToCoordinates();
   // Eat chess and move chess
   if (dataPlayChess[0] == 2)
   {
     Serial.println("Eat and move the chess");
     Serial.println("Step1: Eatting the chess");
-    moveChess(dataPlayChess[2], dataPlayChess[3], coordinatesChessEat[numberChessEat][0], coordinatesChessEat[numberChessEat][1]);
-    numberChessEat++;
+    // moveChess(dataPlayChess[3], dataPlayChess[4], coordinatesChessEat[numberChessEat][0], coordinatesChessEat[numberChessEat][1]);
+    // numberChessEat++;
     
-    delay(200);
+    // delay(200);
 
-    Serial.print("Step2: Moving the chess");
-    moveChess(dataPlayChess[0], dataPlayChess[1], dataPlayChess[2], dataPlayChess[3]);
-  }
-  // Move normal
-  else if (dataPlayChess[0] == 2)
-  {
-    Serial.print("Only move the chess");
-    moveChess(dataPlayChess[0], dataPlayChess[1], dataPlayChess[2], dataPlayChess[3]);
+    // Serial.print("Step2: Moving the chess");
+    // moveChess(dataPlayChess[1], dataPlayChess[2], dataPlayChess[3], dataPlayChess[4]);
+  // }
+  // // Move normal
+  // else if (dataPlayChess[0] == 2)
+  // {
+  //   Serial.print("Only move the chess");
+  //   moveChess(dataPlayChess[1], dataPlayChess[2], dataPlayChess[3], dataPlayChess[4]);
   }
 }
 
@@ -189,8 +232,6 @@ void moveChess(int Coordinates_X0, int Coordinates_Y0, int Coordinates_X1, int C
     Serial.println("Step5: Move to start position");
     // resetPosition();
     moveMotor(0, 0);
-    
-    bPlayChess = false;
 }
 
 void caculateRotationAngle(int x, int y) {
