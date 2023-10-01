8f28647ad5d7e06b1734c0bc2621f99aa5cf08f4
LocNguyen <74259097+locnx-iotvn@users.noreply.github.com>
2023-10-01 18:52:34 +0700

    Arduino
    updated main

--- Arduino/src/main.cpp
+++ Arduino/src/main.cpp
@@ -10,38 +10,29 @@ int stepper1Position, stepper2Position;
 
 String content = "";
 String dataFromUart[NUMBER_DATA_SERIAL];
-int dataPlayChess[5];   // Status, Coordinates_X0, Coordinates_Y0, Coordinates_X1, Coordinates_Y1
-int dataPlayChess_Bk[5];   // Status, Coordinates_X0, Coordinates_Y0, Coordinates_X1, Coordinates_Y1
+int dataPlayChess[7];   // Status, Coordinates_X0, Coordinates_Y0, Coordinates_X1, Coordinates_Y1
 uint8_t numberChessEat = 0;
 
 void startSetup();
 void resetPosition();
-void readDataFromUart();\
-void convertDataUartToCoordinates();
+void readDataFromUart();
 void playChess();
 void moveChess(int Coordinates_X0, int Coordinates_Y0, int Coordinates_X1, int Coordinates_Y1);
+void moveChessTest();
+void moveChessTest2();
 void caculateRotationAngle(int x, int y);
 void moveMotor(double theta1_, double theta2_);
 void takeChessPieces();
 void dropChessPieces();
 void controlServo(uint8_t uiAngle);
+void playChessDemo();
+void moveChessDemo(double theta1_, double theta2_, double theta3_, double theta4_);
 
 void setup() {
   startSetup();
 }
 
 void loop() {
-    if (digitalRead(LIMIT_SWITCH_X) != LIMIT_SWITCH_PRESS)
-    {
-      Serial.println("digitalRead(LIMIT_SWITCH_X)");
-    }
-    if (digitalRead(LIMIT_SWITCH_Y) != LIMIT_SWITCH_PRESS)
-    {
-      Serial.println("digitalRead(LIMIT_SWITCH_Y)");
-    }    
-    delay(1000);
-
-  // Read data
   // readDataFromUart();
 
   // if (bPlayChess == true)
@@ -49,6 +40,20 @@ void loop() {
   //   playChess();
   //   bPlayChess = false;
   // }
+  // takeChessPieces();
+  // delay(1000);
+
+  // moveChessTest();
+  // takeChessPieces();
+  // delay(1000);
+
+  readDataFromUart();
+
+  if (bPlayChess == true)
+  {
+    playChessDemo();
+    bPlayChess = false;
+  }
 }
 
 void startSetup()
@@ -57,20 +62,20 @@ void startSetup()
   Serial.begin(9600);
 
   // Start step motor 1
-  stepper1.setMaxSpeed(4000);
-  stepper1.setAcceleration(2000);
-  // stepper1.setSpeed(100);
-  // stepper1.setAcceleration(100);
+  stepper1.setMaxSpeed(200);
+  stepper1.setAcceleration(100);
+  stepper1.setSpeed(80);
+  stepper1.setAcceleration(80);
 
   // Start step motor 2
-  stepper2.setMaxSpeed(4000);
-  stepper2.setAcceleration(2000);
-  // stepper2.setSpeed(100);
-  // stepper2.setAcceleration(100);
+  stepper2.setMaxSpeed(200);
+  stepper2.setAcceleration(100);
+  stepper2.setSpeed(80);
+  stepper2.setAcceleration(80);
 
   // Start servo motor
   myservo.attach(SERVO_PIN);
-  controlServo(0);
+
 
   // Setup pin output for sucking motor
   pinMode(SUCKING_MOTOR_PIN, OUTPUT);
@@ -82,129 +87,106 @@ void startSetup()
   pinMode(LIMIT_SWITCH_X, INPUT_PULLUP);
   pinMode(LIMIT_SWITCH_Y, INPUT_PULLUP);
 
-  resetPosition();
+  // resetPosition();
+  controlServo(0);
 }
 
 void resetPosition() {
-  delay(500);
+  delay(100);
   // Homing step motor X
   while (digitalRead(LIMIT_SWITCH_X) != LIMIT_SWITCH_PRESS) {
-    stepper1.setSpeed(-130);
+    stepper1.setSpeed(-60);
     stepper1.runSpeed();
-    // stepper1.setCurrentPosition(2000); // When limit switch pressed set position to 0 steps
+    stepper1.setCurrentPosition(0); // When limit switch pressed set position to 0 steps
   }
   delay(20);
-
-  // stepper1.moveTo(0);
-  // while (stepper1.currentPosition() != 0) {
-  //   stepper1.run();
-  // }
 
   // Homing step motor Y
   while (digitalRead(LIMIT_SWITCH_Y) != LIMIT_SWITCH_PRESS) {
-    stepper2.setSpeed(130);
+    stepper2.setSpeed(60);
     stepper2.runSpeed();
-    // stepper1.setCurrentPosition(100);
-  }
-
-  while (digitalRead(LIMIT_SWITCH_Y) != LIMIT_SWITCH_PRESS) {
-    stepper2.setSpeed(20);
-    stepper2.runSpeed();
-    // stepper1.setCurrentPosition(100);
-  }
-
-  while (digitalRead(LIMIT_SWITCH_X) != LIMIT_SWITCH_PRESS) {
-    stepper1.setSpeed(-20);
-    stepper1.runSpeed();
-    // stepper1.setCurrentPosition(2000); // When limit switch pressed set position to 0 steps
+    stepper2.setCurrentPosition(0);
+    // stepper2.setCurrentPosition(120);
   }
   delay(20);
 
-  // stepper2.moveTo(0);
-  // while (stepper2.currentPosition() != 0) {
-  //   stepper2.run();
-  // }
+  while (digitalRead(LIMIT_SWITCH_Y) != LIMIT_SWITCH_PRESS) {
+    stepper2.setSpeed(10);
+    stepper2.runSpeed();
+    stepper2.setCurrentPosition(0);
+  }
+
+  while (digitalRead(LIMIT_SWITCH_X) != LIMIT_SWITCH_PRESS) {
+    stepper1.setSpeed(-10);
+    stepper1.runSpeed();
+    stepper1.setCurrentPosition(0); // When limit switch pressed set position to 0 steps
+  }
+  delay(20);
+
+  Serial.println("stepper1.currentPosition(): " + String(stepper1.currentPosition()));
+  Serial.println("stepper2.currentPosition(): " + String(stepper2.currentPosition()));
 }
 
 void readDataFromUart()
 {
   if (Serial.available()) {
     content = Serial.readString(); // Read the incomding data from Processing
-    for (int i = 0; i < NUMBER_DATA_SERIAL; i++) {
+    // Extract the data from the string and put into separate integer variables (data[] array)
+    for (int i = 0; i < 7; i++) {
       int index = content.indexOf(","); // locate the first ","
-      dataFromUart[i] = content.substring(0, index);
+      dataPlayChess[i] = atol(content.substring(0, index).c_str()); //Extract the number from start to the ","
       content = content.substring(index + 1); //Remove the number from the string
-      Serial.println(dataFromUart[i]);
     }
-
-    /*
-     data[0] - status 
-     data[1] - XY1 position
-     data[2] - XY2 position
-    */
-
     bPlayChess = true;
   }
 }
 
-void convertDataUartToCoordinates()
-{
-  char dataFromUart_temp[3];
-  char posstionChess_temp[3];
-  strcpy(dataFromUart_temp, dataFromUart[0].c_str());
-
-  // Convert Uart data to chess playing data
-  Serial.println(dataFromUart[0]);
-  dataPlayChess[0] = atol(dataFromUart[0].c_str());
-
-  // Coordinates X of the chess
-  for (int i = 0; i < NUMBER_POSTION_CHESS; ++i) {
-    strcpy(posstionChess_temp, posstionChess[i].c_str());
-    if ((dataFromUart_temp[0] == posstionChess_temp[0]) && (dataFromUart_temp[1] == posstionChess_temp[1]) && (dataFromUart_temp[2] == posstionChess_temp[2]))
-    // if (posstionChess[i] == dataFromUart[1])
-    {
-      dataPlayChess[1] = coordinatesChess[i][0];
-      dataPlayChess[2] = coordinatesChess[i][1];
-      Serial.println(i);
-      break;
-    }
-  }
-  Serial.println(dataFromUart[0]);
-
-  // // Coordinates Y of the chess
-  // for (int i = 0; i < NUMBER_POSTION_CHESS; ++i) {
-  //   if (posstionChess[i] == dataFromUart[2])
-  //   {
-  //     dataPlayChess[3] = coordinatesChess[i][0];
-  //     dataPlayChess[4] = coordinatesChess[i][1];
-  //     Serial.println(i);
-  //     break;
-  //   }
-  // }
-  // Serial.println(dataFromUart[0]);
-}
-
 void playChess()
 {
-  convertDataUartToCoordinates();
-  // Eat chess and move chess
   if (dataPlayChess[0] == 2)
   {
     Serial.println("Eat and move the chess");
-    Serial.println("Step1: Eatting the chess");
-    // moveChess(dataPlayChess[3], dataPlayChess[4], coordinatesChessEat[numberChessEat][0], coordinatesChessEat[numberChessEat][1]);
-    // numberChessEat++;
-    
-    // delay(200);
+    Serial.println("Step1: Eatting the chess from(" + String(dataPlayChess[3]) + "," + String(dataPlayChess[4]) + ") to (" + String(dataPlayChess[5]) + "," + String(dataPlayChess[6]) + ")");
 
-    // Serial.print("Step2: Moving the chess");
+    moveChess(dataPlayChess[3], dataPlayChess[4], dataPlayChess[5], dataPlayChess[6]);
+    
+    delay(200);
+
+    Serial.println("Step2: Moving the chess from (" + String(dataPlayChess[1]) + "," + String(dataPlayChess[2]) + ") to (" + String(dataPlayChess[3]) + "," + String(dataPlayChess[4]) + ")");
+    moveChess(dataPlayChess[1], dataPlayChess[2], dataPlayChess[3], dataPlayChess[4]);
+  }
+
+  // Move normal
+  else if (dataPlayChess[0] == 3)
+  {
+    Serial.println("Moving the chess");
+    Serial.println("Only moving the chess from (" + String(dataPlayChess[1]) + "," + String(dataPlayChess[2]) + ") to (" + String(dataPlayChess[3]) + "," + String(dataPlayChess[4]) + ")");
+    moveChess(dataPlayChess[1], dataPlayChess[2], dataPlayChess[3], dataPlayChess[4]);
+  }
+}
+
+void playChessDemo()
+{
+  if (dataPlayChess[0] == 2)
+  {
+    Serial.println("Eat and move the chess");
+    // Serial.println("Step1: Eatting the chess from(" + String(dataPlayChess[3]) + "," + String(dataPlayChess[4]) + ") to (" + String(dataPlayChess[5]) + "," + String(dataPlayChess[6]) + ")");
+
+    moveChessDemo(76,-44,25,-43);
+    
+    delay(200);
+
+    // Serial.println("Step2: Moving the chess from (" + String(dataPlayChess[1]) + "," + String(dataPlayChess[2]) + ") to (" + String(dataPlayChess[3]) + "," + String(dataPlayChess[4]) + ")");
+    moveChessDemo(53,-27,76,-44);
+  }
+
+  // Move normal
+  else if (dataPlayChess[0] == 3)
+  {
+    Serial.println("Moving the chess");
+    // Serial.println("Only moving the chess from (" + String(dataPlayChess[1]) + "," + String(dataPlayChess[2]) + ") to (" + String(dataPlayChess[3]) + "," + String(dataPlayChess[4]) + ")");
     // moveChess(dataPlayChess[1], dataPlayChess[2], dataPlayChess[3], dataPlayChess[4]);
-  // }
-  // // Move normal
-  // else if (dataPlayChess[0] == 2)
-  // {
-  //   Serial.print("Only move the chess");
-  //   moveChess(dataPlayChess[1], dataPlayChess[2], dataPlayChess[3], dataPlayChess[4]);
+    moveChessDemo(44,-26,75,-35);
   }
 }
 
@@ -230,8 +212,80 @@ void moveChess(int Coordinates_X0, int Coordinates_Y0, int Coordinates_X1, int C
 
     // Drop the chess pieces
     Serial.println("Step5: Move to start position");
-    // resetPosition();
-    moveMotor(0, 0);
+    resetPosition();
+    // moveMotor(0, 0);
+}
+
+void moveChessTest()
+{
+  if (Serial.available()) {
+    content = Serial.readString(); // Read the incomding data from Processing
+    // Extract the data from the string and put into separate integer variables (data[] array)
+    for (int i = 0; i < 4; i++) {
+      int index = content.indexOf(","); // locate the first ","
+      dataPlayChess[i] = atol(content.substring(0, index).c_str()); //Extract the number from start to the ","
+      content = content.substring(index + 1); //Remove the number from the string
+    }
+    bPlayChess = true;
+  }
+
+  if (bPlayChess == true)
+  {
+    // Move motor to the position of chess pieces 
+    Serial.println("Step1: Move to the position of chess");
+    moveMotor(dataPlayChess[0], dataPlayChess[1]);
+    Serial.print("theta11: "); Serial.println(dataPlayChess[0]);
+    Serial.print("theta21: "); Serial.println(dataPlayChess[1]);
+
+    // Take the chess pieces
+    Serial.println("Step2: Take the chess pieces");
+    // delay(2000);
+    takeChessPieces();
+  
+    // Move chess pieces to next postion
+    Serial.println("Step3: Move chess pieces to next postion");
+    moveMotor(dataPlayChess[2], dataPlayChess[3]);
+    Serial.print("theta11: "); Serial.println(dataPlayChess[2]);
+    Serial.print("theta21: "); Serial.println(dataPlayChess[3]);
+    // delay(2000);
+
+    // Drop the chess pieces
+    Serial.println("Step4: Drop the chess pieces");
+    dropChessPieces();
+
+    // Drop the chess pieces
+    Serial.println("Step5: Move to start position");
+    resetPosition();
+
+    bPlayChess = false;
+  }
+}
+
+void moveChessDemo(double theta1_, double theta2_, double theta3_, double theta4_)
+{
+    // Move motor to the position of chess pieces 
+    Serial.println("Step1: Move to the position of chess");
+    moveMotor(theta1_, theta2_);
+    Serial.print("theta11: "); Serial.println(theta1_);
+    Serial.print("theta21: "); Serial.println(theta2_);
+
+    // Take the chess pieces
+    Serial.println("Step2: Take the chess pieces");
+    takeChessPieces();
+  
+    // Move chess pieces to next postion
+    Serial.println("Step3: Move chess pieces to next postion");
+    moveMotor(theta3_, theta4_);
+    Serial.print("theta11: "); Serial.println(theta3_);
+    Serial.print("theta21: "); Serial.println(theta4_);
+
+    // Drop the chess pieces
+    Serial.println("Step4: Drop the chess pieces");
+    dropChessPieces();
+
+    // Drop the chess pieces
+    Serial.println("Step5: Move to start position");
+    resetPosition();
 }
 
 void caculateRotationAngle(int x, int y) {
@@ -261,11 +315,16 @@ void caculateRotationAngle(int x, int y) {
     theta1 = 270 + theta1;
   }
 
-  theta2 = theta2 - 90;
+  theta2 = theta2 + 69;
 
   theta1=round(theta1);
   theta2=round(theta2);
   
+  Serial.print("theta1: "); Serial.println(theta1);
+  Serial.print("theta2: "); Serial.println(theta2);
+}
+
+void convertCoordinatesToAngle(int x, int y) {
   Serial.print("theta1: "); Serial.println(theta1);
   Serial.print("theta2: "); Serial.println(theta2);
 }
